8a9bb4049c496ddd18f4e5a25f602cbac2e49ff2
kigorekm <kigorekm@gmail.com>
2021-01-18 11:09:07 +0300

    testing

--- src/Testing/task2.java
+++ src/Testing/task2.java
@@ -1,12 +1,12 @@
 package Testing;
 import java.util.*;
-public class task{
+public class task2 {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
         System.out.print("vvedi one: ");
         double one = input.nextDouble();
-        double two = one * 5;
+        double two = one * 6;
         System.out.println("two = " + two);
         double three = one + two;
         System.out.println("three = " + three);
